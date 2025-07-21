c88207860622307baa6d580f52c98069afbe6be7
vishal-0-12 <vishalathwal2003@gmail.com>
2025-07-21 11:01:13 +0530

    backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.', // assume index.html is in frontend/
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist', // required for Vercel
   },
   server: {
     proxy: {
